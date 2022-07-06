3e3d8271eaa8025ac5987f1b5c66e3ea65df09e1
Jalilov <javohirjalilov777@gmail.com>
2022-07-06 22:04:01 +0500

    added tasks

---
+++ arithmetic_op05.py
@@ -0,0 +1,7 @@
+#Create a variable called 'number' with data type int.
+ 
+#Create a variable called 'answer'
+
+#Find the remainder of the division of number by 3.
+ 
+#Print the answer
---
+++ arithmetic_op06.py
@@ -0,0 +1,9 @@
+#Create a variable called 'number' with data type int.
+ 
+#Divide the number by 2 and assign number.
+ 
+#Find the fraction of the number and assing to a variable called 'answer'.
+ 
+#Print the answer.
+ 
+#Print the number.
