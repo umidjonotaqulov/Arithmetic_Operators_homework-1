cff0018e92d9f73c3268b0504599dce24ce21b6f
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-07 12:49:33 +0500

    x6

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,9 +1,15 @@
 #Create a variable called 'number' with data type int.
  
 #Divide the number by 2 and assign number.
- 
+
 #Find the fraction of the number and assing to a variable called 'answer'.
  
 #Print the answer.
  
 #Print the number.
+
+number=19
+
+x=number/2-number//2
+
+print(x)
