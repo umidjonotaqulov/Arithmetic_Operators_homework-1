0da63ad967b392fd9c1f2c3ccd4b38469096c432
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-07 10:42:27 +0500

    x3

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -3,3 +3,14 @@
 #Find the reverse of the number and assign it to a variable called 'answer'.
 
 #Print the answer variable
+
+number=89
+
+x1=number%10
+x1*=10
+
+x2=number//10
+
+answer=x1+x2
+
+print(answer)
